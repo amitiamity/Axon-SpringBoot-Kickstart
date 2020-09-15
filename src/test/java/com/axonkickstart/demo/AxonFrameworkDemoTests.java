8031e3b1a88fc58ac8e2c35fa196c9c42e351d02
amitiamity <amitptl.in@gmail.com>
2020-09-15 23:59:15 +0400

    initial project setup and dependencies

---
+++ src/test/java/com/axonkickstart/demo/AxonFrameworkDemoTests.java
@@ -0,0 +1,13 @@
+package com.axonkickstart.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonFrameworkDemoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
